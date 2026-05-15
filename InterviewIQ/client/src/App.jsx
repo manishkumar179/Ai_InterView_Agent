35f21b33fe5d9cc6d827f33ccd446c57942c0129
manishkumar179 <mk0437271@gmail.com>
2026-05-15 21:36:42 +0530

    Fetched current user

--- InterviewIQ/client/src/App.jsx
+++ InterviewIQ/client/src/App.jsx
@@ -2,10 +2,24 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Auth from './pages/Auth'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 export const ServerUrl = "http://localhost:8000"
 
 const App = () => {
+  useEffect(()=>{
+    const getUser = async ()=>{
+      try {
+        const result = await axios.get(ServerUrl+"/api/user/current-user" , {withCredentials:true})
+        console.log(result.data.user);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getUser()
+  },[])
   return (
     <Routes>
       <Route path='/' element={<Home/>} />
