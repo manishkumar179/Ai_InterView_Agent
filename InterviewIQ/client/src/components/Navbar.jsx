2ff0956202eb5bdb9e3feb90fb1af818657b9a5a
manishkumar179 <mk0437271@gmail.com>
2026-05-27 22:59:41 +0530

    Create home page and show some popup if user are not login

--- InterviewIQ/client/src/components/Navbar.jsx
+++ InterviewIQ/client/src/components/Navbar.jsx
@@ -1,13 +1,35 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { motion } from "motion/react";
 import { BsRobot, BsCoin } from "react-icons/bs";
 import { HiOutlineLogout } from "react-icons/hi";
 import { FaUserAstronaut } from "react-icons/fa";
+import {useNavigate} from 'react-router-dom'
+import axios from 'axios'
+import { ServerUrl } from "../App";
+import { setUserData } from "../redux/userSlice";
+import AuthModel from "./AuthModel";
 
 const Navbar = () => {
   const { userData } = useSelector((state) => state.user);
-  console.log( "userdata ---" , userData)
+  // console.log( "userdata ---" , userData)
+  let navigate = useNavigate()
+  let dispatch = useDispatch()
+  const [showCreditPopup , setShowCreditPopup] =  useState(false);
+  const [showUserPopup , setShowUserPopup] =  useState(false);
+  const [showAuth , setShowAuth] = useState(false)
+
+  const handleLogout = async ()=>{
+    try {
+      await axios.get(ServerUrl + "/api/auth/logout" , {withCredentials:true})
+      dispatch(setUserData(null))
+      setShowCreditPopup(false)
+      setShowUserPopup(false)
+      navigate("/")
+    } catch (error) {
+      console.log("Error in logout function api:  ",error)
+    }
+  }
 
   return (
     <div className=" bg-[#f3f3f3] flex justify-center px-4 pt-6 ">
@@ -30,21 +52,66 @@ const Navbar = () => {
 
         <div className=" flex items-center gap-6 relative ">
           <div className=" relative ">
-            <button className=" flex items-center gap-2 bg-gray-100 px-4 py-2 
+
+            <button onClick={()=>{
+              if(!userData){
+                setShowAuth(true)
+                return;
+              }
+              setShowCreditPopup(!showCreditPopup);
+              setShowUserPopup(false)
+            }} className=" flex items-center gap-2 bg-gray-100 px-4 py-2 
             rounded-full text-md hover:bg-gray-200 transition ">
                 <BsCoin size={20} />
                 {userData?.user.credits || 0 }
             </button>
+
+            {/* Credit popup */}
+            {showCreditPopup && (
+              <div className=" absolute right-[-50px] mt-3 w-64 bg-white shadow-xl border border-gray-200 rounded p-5 z-50 " >
+                <p className="text-sm text-gray-600 mb-4" >Need more credits to continue interview ?</p>
+                <button onClick={()=>navigate("/pricing")} className=" w-full bg-black text-white py-2 rounded-lg text-sm  "> Buy more credits </button>
+              </div> 
+            )}
+
+
+
           </div>
 
          <div className=" relative ">
-            <button className=" w-9 h-9 bg-black text-white rounded-full
+            <button onClick={()=>{
+               if(!userData){
+                setShowAuth(true)
+                return;
+              }
+              setShowUserPopup(!showUserPopup);
+              setShowCreditPopup(false);
+            }} className=" w-9 h-9 bg-black text-white rounded-full
              flex items-center justify-center font-semibold ">
                 {userData ? userData.user.name.slice(0,1).toUpperCase() : <FaUserAstronaut size={16}/>  }
             </button>
+
+
+            {/* user POPup */}
+            {showUserPopup && (
+
+              <div className=" absolute right-0 mt-3 w-48 bg-white shadow-xl border border-gray-200 rounded-xl p-4 z-50 " >
+
+                <p className="text-md text-blue-500 font-medium mb-1">{userData?.user.name}</p>
+                <button onClick={()=>navigate("/history")} className="w-ful text-left text-sm py-2 hover:text-black text-gray-600 " > Interview History </button>
+                <button onClick={handleLogout} className=" w-ful text-left text-sm py-2 flex items-center gap-2 text-red-500   " >
+                  <HiOutlineLogout size={16} />
+                  Logout</button>
+
+
+              </div>
+            )}
+
+
           </div>
         </div>
       </motion.div>
+      {showAuth && <AuthModel onClose={()=>setShowAuth(false)} />}
     </div>
   );
 };
--- InterviewIQ/client/src/pages/Home.jsx
+++ InterviewIQ/client/src/pages/Home.jsx
@@ -1,12 +1,61 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
+import { useSelector } from "react-redux";
+import { motion } from "motion/react";
+import {
+  BsRobot,
+  BsMic,
+  BsClock,
+  BsBarChart,
+  BsFileEarmarkText,
+} from "react-icons/bs";
+
+import { HiSparkles } from "react-icons/hi";
 
 const Home = () => {
+  const { userData } = useSelector((state) => state.user);
   return (
-    <div className='min-h-screen bg-[#f3f3f3] flex flex-col'>
-      <Navbar/>
-    </div>
-  )
-}
+    <div className="min-h-screen bg-[#f3f3f3] flex flex-col">
+      <Navbar />
 
-export default Home
+      <div className=" flex-1 px-6 py-20 ">
+        <div className="flex justify-center mb-6  ">
+          <div
+            className=" bg-gray-100 text-gray-600 text-sm px-4 py-2 
+           rounded-full flex items-center gap-2 "
+          >
+            <HiSparkles size={16} className=" bg-green-50 text-green-600 " />
+            AI Powered Smart Interview Platform
+          </div>
+        </div>
+        <div className=" text-center mb-28 " >
+            <motion.h1 
+            initial={{opacity:0 , y:30}}
+            animate={{opacity:1 , y:0}}
+            transition={{duration:0.6}}
+            className=" text-4xl md:text-6xl font-semibold leading-tight max-w-4xl mx-auto " >
+              Practice Interviews with
+
+              <span className=" relative inline-block ">
+                <span className=" bg-green-100 text-green-600 px-5 py-1 rounded-full " >
+                  AI Intelligence
+                </span>
+              </span>
+            </motion.h1>
+
+            <motion.p 
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            transition={{duration:0.8}}
+            className=" text-gray-500 mt-6 max-w-2xl mx-auto text-lg ">
+              Role-based mock interviews with smart follow-ups,
+              adaptive difficulty and real-time performance
+              evaluation. 
+            </motion.p>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
