b01c0fea33f8ec3052309c332cae5fda88cf14f6
manishkumar179 <mk0437271@gmail.com>
2026-08-14 01:00:30 +0530

    Download pdf feature added

--- InterviewIQ/client/src/components/Step3Report.jsx
+++ InterviewIQ/client/src/components/Step3Report.jsx
@@ -14,6 +14,9 @@ import {
   YAxis,
 } from "recharts";
 
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
+
 const Step3Report = ({ report }) => {
   let navigate = useNavigate();
   if (!report) {
@@ -59,6 +62,125 @@ const Step3Report = ({ report }) => {
   const score = finalScore;
   const percentage = (score / 10) * 100;
 
+  // Download pdf function (used jspdf for pdf creation)
+  const downloadPDF = () => {
+    const doc = new jsPDF("p", "mm", "a4");
+
+    const pageWidth = doc.internal.pageSize.getWidth();
+    const margin = 20;
+    const contentWidth = pageWidth - margin * 2;
+
+    let currentY = 25;
+
+    // Title
+
+    doc.setFont("helvetica", "bold");
+    doc.setFontSize(20);
+    doc.setTextColor(34, 197, 94);
+    doc.text("AI Interview Performance Report", pageWidth / 2, currentY, {
+      align: "center",
+    });
+
+    currentY += 5;
+
+    // Underline
+
+    doc.setDrawColor(34, 197, 94);
+    doc.line(margin, currentY + 2, pageWidth - margin, currentY + 2);
+
+    currentY += 15;
+
+    // Final score box
+
+    doc.setFillColor(240, 253, 244);
+    doc.roundedRect(margin, currentY, contentWidth, 20, 4, 4, "F");
+    doc.setFontSize(14);
+    doc.setTextColor(0, 0, 0);
+    doc.text(`Final Score:${finalScore}/10 `, pageWidth / 2, currentY + 12, {
+      align: "center",
+    });
+
+    currentY += 30;
+
+    // ====================== SKILL BOX =================
+
+    doc.setFillColor(249, 250, 251);
+    doc.roundedRect(margin, currentY, contentWidth, 30, 4, 4, "F");
+
+    doc.setFontSize(12);
+
+    doc.text(`Confidence:${confidence}`, margin + 10, currentY + 10);
+    doc.text(`Communication: ${communication}`, margin + 10, currentY + 18);
+    doc.text(`Correctness: ${correctness}`, margin + 10, currentY + 26);
+
+    currentY += 45;
+
+    //=================  ADVICE ======================
+
+    let advice = "";
+
+    if (finalScore >= 8) {
+      advice =
+        "Excellent performance. Maintain confidence and structure. Continue refining clarity and supporting answers with strong real-world examples. ";
+    } else if (finalScore >= 5) {
+      advice =
+        "Good foundation shown. Improve clarity and structure. Practice delivering concise, confident answer with stronger supporting examples.";
+    } else {
+      advice =
+        "Significant improvement required. Focus on structured thinking, clarity, and confident delivering. Practice answering aloud regularly.";
+    }
+
+    doc.setFillColor(255, 255, 255);
+    doc.setDrawColor(220);
+    doc.roundedRect(margin, currentY, contentWidth, 35, 4, 4);
+
+    doc.setFont("helvetica", "bold");
+    doc.text("Professional Advice", margin + 10, currentY + 10);
+
+    doc.setFont("helvetica", "normal");
+    doc.setFontSize(11);
+
+    const spliteAdvice = doc.splitTextToSize(advice, contentWidth - 20);
+    doc.text(spliteAdvice, margin + 10, currentY + 20);
+
+    currentY += 50;
+
+    //================ QUESTION TABLE ============
+
+    autoTable(doc, {
+      startY: currentY,
+      margin: { left: margin, right: margin },
+      head: [["#", "Question", "Score", "Feedback"]],
+      body: questionWiseScore.map((q, i) => [
+        `${i + 1}`,
+        q.question,
+        `${q.score}/10`,
+        q.feedback,
+      ]),
+      styles: {
+        fontSize: 9,
+        cellPadding: 5,
+        valign: "top",
+      },
+      headStyles: {
+        fillColor: [34, 197, 94],
+        textColor: 255,
+        halign: "center",
+      },
+      columnStyles: {
+        0: { cellWidth: 10, halign: "center" }, //index
+        1: { cellWidth: 55 }, //question
+        2: { cellWidth: 20, halign: "center" }, //score
+        3: { cellWidth: "auto" }, // feedback
+      },
+      alternateRowStyles: {
+        fillColor: [249, 250, 251],
+      },
+    });
+
+    doc.save("AI_Interview_Report.pdf");
+  };
+
   return (
     <div
       className="min-h-screen bg-linear-to-br from-gray-50 to-green-50 
@@ -89,13 +211,16 @@ const Step3Report = ({ report }) => {
           </div>
         </div>
 
+        {/* Download pdf button */}
         <button
+          onClick={downloadPDF}
           className=" bg-emerald-600 hover:bg-emerald-700 text-white px-3
       py-3 rounded-xl shadow-md transition-all duration-300
       font-semibold text-sm sm:text-base text-nowrap "
         >
           Download PDF
         </button>
+        
       </div>
 
       {/* Progress Bar of report */}
@@ -231,13 +356,8 @@ const Step3Report = ({ report }) => {
                     className=" flex flex-col sm:flex-row sm:justify-between 
                   sm:items-start gap-3 mb-4 "
                   >
-
-
                     <div>
-
-                      <p className="text-xs text-gray-400 ">
-                        Question {i + 1}
-                      </p>
+                      <p className="text-xs text-gray-400 ">Question {i + 1}</p>
                       <p
                         className="font-semibold text-gray-800 text-sm
                       sm:text-base leading-relaxed "
@@ -246,25 +366,27 @@ const Step3Report = ({ report }) => {
                       </p>
                     </div>
 
-                    <div className="bg-green-100 text-green-600 px-3 py-1
-                    rounded-full font-bold text-xs sm:text-sm w-fit  ">
+                    <div
+                      className="bg-green-100 text-green-600 px-3 py-1
+                    rounded-full font-bold text-xs sm:text-sm w-fit  "
+                    >
                       {q.score ?? 0}/10
                     </div>
-
-
                   </div>
 
-                  <div className="bg-green-50 border-green-200 p-4
-                   rounded-lg ">
-                    <p className="text-xs text-green-600 font-semibold mb-1 ">AI Feedback</p>
+                  <div
+                    className="bg-green-50 border-green-200 p-4
+                   rounded-lg "
+                  >
+                    <p className="text-xs text-green-600 font-semibold mb-1 ">
+                      AI Feedback
+                    </p>
                     <p className="text-sm text-gray-700 leading-relaxed ">
                       {q.feedback && q.feedback.trim() !== ""
-                      ? q.feedback : "No feedback available for this question." }
+                        ? q.feedback
+                        : "No feedback available for this question."}
                     </p>
                   </div>
-
-
-
                 </div>
               ))}
             </div>
