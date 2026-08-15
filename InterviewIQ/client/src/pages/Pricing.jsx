60faf02d950b07658cc82a58ed2f9354dc47c146
manishkumar179 <mk0437271@gmail.com>
2026-08-15 11:23:34 +0530

    Created download pdf features and start implemented to purchase more credits

--- InterviewIQ/client/src/pages/Pricing.jsx
+++ InterviewIQ/client/src/pages/Pricing.jsx
@@ -1,11 +1,155 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaArrowLeft } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { motion, scale } from "motion/react";
 
 const Pricing = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [selectedPlan, setSelectedPlan] = useState("free");
 
-export default Pricing
+  const plans = [
+    {
+      id: "free",
+      name: "Free",
+      price: "₹0",
+      credits: 100,
+      description: "Perfect for begineers starting interview preparation.",
+      features: [
+        "100 AI Interview Credits",
+        "Basics Performance Report",
+        "Voice Interview Access",
+        "Limited History Tracking",
+      ],
+      default: true,
+    },
+
+    {
+      id: "basic",
+      name: "Starter Pack",
+      price: "₹100",
+      credits: 150,
+      description: "Great for focused practice and skill improvement.",
+      features: [
+        "150 AI Interview Credits",
+        "Detailed Feedback",
+        "Performance Analytics",
+        "Full Interview History",
+      ],
+    },
+    {
+      id: "pro",
+      name: "Pro Pack",
+      price: "₹500",
+      credits: 650,
+      description: "Best value for serious job preparation.",
+      features: [
+        "650 AI Interview Credits",
+        "Advanced AI Feedback",
+        "Skill Trend Analysis",
+        "Priority AI Processing",
+      ],
+      badge: "Best Value",
+    },
+  ];
+
+  const navigate = useNavigate();
+  return (
+    <div
+      className="min-h-screen bg-gradient-to-br from-gray-50 to-emerald-50
+    py-16 px-6 "
+    >
+      {/* Back arrow and choose your plan */}
+      <div className=" max-w-6xl mx-auto mb-14 flex items-start gap-4">
+        <button
+          onClick={() => navigate("/")}
+          className="mt-2 p-3 rounded-full bg-white shadow hover:shadow-md transition "
+        >
+          <FaArrowLeft className="text-gray-600" />
+        </button>
+
+        <div className="text-center w-full ">
+          <h1 className="text-4xl font-bold text-gray-800 ">
+            Choose Your Plane
+          </h1>
+
+          <p className="text-gray-500 mt-3 text-lg ">
+            Flexible pricing to match your interview preparation goals.
+          </p>
+        </div>
+      </div>
+
+      <div
+        className=" grid md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl
+      mx-auto "
+      >
+        {plans.map((plan) => {
+          const isSelected = selectedPlan == plan.id;
+
+          return (
+            <motion.div
+              key={plan.id}
+              whileHover={!plan.default && { scale: 1.03 }}
+              onClick={() => !plan.default && setSelectedPlan(plan.id)}
+              className={` relative rounded-3xl p-8 transition-all duration-300
+                border 
+                
+                ${
+                  isSelected
+                    ? "border-emerald-600 shadow-2xl bg-white "
+                    : " border-gray-200 bg-white shadow-md "
+                }
+                ${plan.default ? "cursor-default " : "cursor-pointer "}
+                `}
+            >
+              {/* Badge */}
+
+              {plan.badge && (
+                <div
+                  className=" absolute top-6 right-6 bg-emerald-600 
+                  text-white text-xs px-4 py-1 rounded-full shadow  "
+                >
+                  {plan.badge}
+                </div>
+              )}
+
+              {/* Default Tag */}
+              {plan.default && (
+                <div className=" absolute top-6 right-6 bg-gray-200
+                text-gray-700 text-xs px-3 py-1 rounded-full ">
+                  Default
+                </div>
+              )}
+
+              {/* Plan Name */}
+              <h3 className=" text-xl font-semibold text-gray-800  ">
+                {plan.name}
+              </h3>
+
+              {/* Price */}
+
+              <div className=" mt-4 ">
+                <span className=" text-3xl font-bold text-emerald-600 ">
+                  {plan.price}
+                </span>
+
+                <p className=" text-gray-500 mt-1 ">
+                  {plan.credits} Credits
+                </p>
+
+              </div>
+
+              {/* Description */}
+
+              <p className="  ">
+
+              </p>
+
+
+            </motion.div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Pricing;
