013217acf5c98265995f74eb3de4b8a789060a88
manishkumar179 <mk0437271@gmail.com>
2026-08-26 10:48:58 +0530

    final setup

--- InterviewIQ/server/controllers/auth.controller.js
+++ InterviewIQ/server/controllers/auth.controller.js
@@ -28,8 +28,8 @@ export const googleAuth = async (req, res) => {
     // Store cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
