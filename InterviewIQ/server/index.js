31e5db9375d91b9c0a0bc52051ad1460342904e6
Manish Kumar <mk0437271@gmail.com>
2026-08-26 11:31:04 +0530

    Update CORS origin for production deployment
    Added origin

--- InterviewIQ/server/index.js
+++ InterviewIQ/server/index.js
@@ -15,7 +15,7 @@ let app = express()
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://ai-interview-agent-client-hb8r.onrender.com",
     credentials:true
 }))
 
